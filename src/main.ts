023eebb8197edc1b6817eaedb732ca55329cad2a
Tharindu Dilshan Ekanayake <dilshantharindu8@gmail.com>
2026-05-18 13:03:49 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,11 @@ type DatabaseOptions = {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
